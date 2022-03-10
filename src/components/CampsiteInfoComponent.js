508c34b4964011b6e5b1a7cfb6e8e894d3f93e15
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-03-10 15:38:03 -0500

    6th commit // heeader and footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,61 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class CampsiteInfoComponent extends Component {
-  renderCampsite(campsite) {
+function RenderCampsite({ campsite }) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name}></CardImg>
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function RenderComments({ comments }) {
+  if (comments) {
     return (
       <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name}></CardImg>
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+        <h4>Comments</h4>
+        {comments.map((singleComment) => {
+          return (
+            <div>
+              {singleComment.text} <br />
+              --{singleComment.author}{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(singleComment.date)))}
+            </div>
+          );
+        })}
       </div>
     );
-  }
-
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map((singleComment) => {
-            return (
-              <div>
-                {singleComment.text} <br />
-                --{singleComment.author}{" "}
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "2-digit",
-                }).format(new Date(Date.parse(singleComment.date)))}
-              </div>
-            );
-          })}
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
-  render() {
-    if (this.props.campsite) {
-      console.log(this.props.campsite);
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-          </div>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
+  } else {
+    return <div></div>;
   }
 }
 
-export default CampsiteInfoComponent;
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    console.log(props.campsite);
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite}></RenderCampsite>
+          <RenderComments comments={props.campsite.comments}></RenderComments>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
+
+export default CampsiteInfo;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import { CAMPSITES } from "../shared/campsites";
+
 
 class Main extends Component {
   constructor(props) {
@@ -20,11 +22,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header></Header>
         <Directory
           campsites={this.state.campsites}
           onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
@@ -36,6 +34,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer></Footer>
       </div>
     );
   }
